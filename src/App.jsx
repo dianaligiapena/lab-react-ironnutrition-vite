49711c02c188bd5d859602454075a83ae280d6d1
Diana Pena <dianaligiapena@gmail.com>
2023-02-22 13:20:19 +0200

    1st iteration done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Row, Divider, Button, Col, Card, Input } from 'antd';
+import foods from './foods.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [foodsState, setFoodsState] = useState(foods) // set - method to change
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+                {/* Display Add Food component here */}
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {/* Render the list of Food Box components here */}
+        {foodsState.map( (food) => {
+            return (
+              <Col>
+                <Card
+                  title={food.name}
+                  style={{ width: 230, height: 300, margin: 10 }}
+                >
+                  <img src={food.image} height={60} alt="food" />
+                  <p>Calories: {food.calories}</p>
+                  <p>Servings: {food.servings}</p>
+                  <p>
+                    <b>Total Calories: { food.calories * food.servings } </b> kcal
+                  </p>
+                  <Button type="primary"> Delete </Button>
+                </Card>
+              </Col>
+            )
+
+        } )}
+      </Row>
+
     </div>
   )
 }
 
 export default App
+
